bd826cba21d77db6c2997741214ad8dee8c7ff70
henri mfonzie <henri.gbetkom@yahoo.fr>
2021-06-03 23:11:24 +0200

    installation python avec le Vagrantfile

--- Jenkins/install.sh
+++ Jenkins/install.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update
-
-# install python
-apt-get install python3 python3-dev python3-pip -q -y
-apt remove -y python 
-
-ln -s /usr/bin/python3 /usr/bin/python
-
 dpkg -l | grep jenkins
 if [ $? = 0 ]; then
     echo "Serveur intégration déjà installé"
