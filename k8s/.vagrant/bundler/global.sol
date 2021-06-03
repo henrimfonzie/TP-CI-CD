84dc7dd4d3376082b1c3a83148cf90dd99e31333
Alexandre PALPANT <alexandre.palpant@gmail.com>
2021-06-03 17:23:39 +0200

    k8s

---
+++ k8s/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["net-ssh",[">= 2.6.5"]],["net-scp",["~> 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.7"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.29.0"]]],"checksum":"8b6c19dc0d75c6c773c6af669dfa2faba091cc0b7b6d096d28b7b4bfdb4a5fa2","vagrant_version":"2.2.14"}
