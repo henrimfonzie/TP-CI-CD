b61fbfb29dc549ba41fb2e6a44ca7572f73ba1ad
Alexandre PALPANT <alexandre.palpant@gmail.com>
2021-06-07 10:31:30 +0200

    k8s-ELK

--- k8s-light/provision.sh
+++ k8s-light/provision.sh
@@ -7,3 +7,9 @@ apt install -y ansible
 cd /home/shared/
 
 ansible-playbook -i targets k8s-playbook.yml
+
+cd ./docker-elk/
+
+docker-compose up --build -d
+
+echo "192.168.50.10		logstash" | sudo tee -a /etc/hosts
